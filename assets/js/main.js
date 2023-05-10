061ae0731450c713e275630c1a6cc3ef60b851a9
357642 <dusqls76@naver,com>
2023-05-10 13:47:32 +0900

    css 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ $(function(){
             delay: 1700,
             disableOnInteraction: false
         },
-    })
+    });
     /**
      * @주요제품소개슬라이드
      */
@@ -23,7 +23,7 @@ $(function(){
         pagination:{
             el:'.pagination',
         },
-    })
+    });
     /**
      * @제품슬라이드
      */
@@ -31,7 +31,7 @@ $(function(){
         spaceBetween:10,
         slidesPerView: 2.2,
         freeMode: true,
-    })
+    });
     /**
      * @헤더
      */
@@ -41,9 +41,9 @@ $(function(){
         if (curr > 0) {
             $('.header').addClass('fixed');
         } else {
-            $('.header').removeClass('fixed')
-        }
-    })
+            $('.header').removeClass('fixed');
+        };
+    });
     $(window).trigger('scroll');
     /**
      * @전체메뉴
@@ -51,33 +51,33 @@ $(function(){
     $('.gnb .btn-all').click(function(){
         $('.gnb').toggleClass('open');
         $('.gnb .group-all').slideToggle();
-    })
+    });
     /**
      * @메뉴창
      */
     $('.btn-menu').click(function(){
-        $('.side-nav').addClass('on')
+        $('.side-nav').addClass('on');
         $('body').addClass('hidden');
-    })
+    });
     $('.side-nav .btn-close, .dimmed').click(function(){
-        $('.side-nav').removeClass('on')
+        $('.side-nav').removeClass('on');
         $('body').removeClass('hidden');
-    })
+    });
     /**
      * @검색창
      */
     $('.btn-search').click(function(){
-        $('.popup-search').addClass('on')
-    })
+        $('.popup-search').addClass('on');
+    });
     $('.popup-search .close').click(function(){
-        $('.popup-search').removeClass('on')
-    })
+        $('.popup-search').removeClass('on');
+    });
     /**
      * @아코디언메뉴
      */
     $('.side-nav .nav').click(function(){
         $(this).toggleClass('on').siblings('.side-nav .depth2').stop().slideToggle();
-    })
+    });
     /**
      * @베스트상품메뉴
      */
@@ -88,7 +88,7 @@ $(function(){
 
         $(this).addClass('active').siblings().removeClass('active');
         $(target).addClass('active').siblings().removeClass('active');
-    })
+    });
     /**
      * @고정버튼
     */
@@ -99,17 +99,17 @@ $(function(){
 
         if(curr >= 10){
             if(curr > lastScroll){
-                $('.fix-btn').removeClass('show')
+                $('.fix-btn').removeClass('show');
             }else{
-                $('.fix-btn').addClass('show')
-            }
+                $('.fix-btn').addClass('show');
+            };
         }else{
-            $('.fix-btn').removeClass('show')
-        }
+            $('.fix-btn').removeClass('show');
+        };
         
         lastScroll = curr;
-    })
+    });
     $('.fix-btn .btn-top').click(function(){
-        window.scrollTo({top:0,behavior:"smooth"})
-    })
-})
+        window.scrollTo({top:0,behavior:"smooth"});
+    });
+});
